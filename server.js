ebbba47356edb370b3166dc2a141a665daafd79c
Your NameAlex <alehr45@gmail.com>
2021-02-24 13:35:38 -0600

    combined front and back end

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming string or array data
+
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
@@ -37,6 +37,7 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+
 function findById(id, animalsArray) {
   const result = animalsArray.filter(animal => animal.id === id)[0];
   return result;
@@ -44,7 +45,7 @@ function findById(id, animalsArray) {
 
 function createNewAnimal(body, animalsArray) {
   const animal = body;
-  animalsArray.push(animal);
+  animals.push(animal);
   fs.writeFileSync(
     path.join(__dirname, './data/animals.json'),
     JSON.stringify({ animals: animalsArray }, null, 2)
@@ -68,6 +69,8 @@ function validateAnimal(animal) {
   return true;
 }
 
+
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -85,11 +88,18 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
 app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
@@ -98,6 +108,9 @@ app.post('/api/animals', (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
